210d4a155c5eb233a33c6cefb5c5ae32d7136a9e
LeoPD15 <leon_xlr8@hotmail.com>
2020-10-14 14:04:15 -0500

    Controladores

---
+++ app/Http/Controllers/DetalleRecolectorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DetalleRecolector;
+use Illuminate\Http\Request;
+
+class DetalleRecolectorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DetalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DetalleRecolector $detalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DetalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DetalleRecolector $detalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DetalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DetalleRecolector $detalleRecolector)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DetalleRecolector  $detalleRecolector
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DetalleRecolector $detalleRecolector)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PuntoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PuntoRecoleccion;
+use Illuminate\Http\Request;
+
+class PuntoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PuntoRecoleccion  $puntoRecoleccion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PuntoRecoleccion $puntoRecoleccion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PuntoRecoleccion  $puntoRecoleccion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PuntoRecoleccion $puntoRecoleccion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PuntoRecoleccion  $puntoRecoleccion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PuntoRecoleccion $puntoRecoleccion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PuntoRecoleccion  $puntoRecoleccion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PuntoRecoleccion $puntoRecoleccion)
+    {
+        //
+    }
+}
