b52188aa1f275f7d3b3639fcba3124e5780c2754
LeoPD15 <leon_xlr8@hotmail.com>
2020-10-17 01:01:24 -0500

    adiciones Materialize

--- resources/views/detalleRecolector.blade.php
+++ resources/views/detalleRecolector.blade.php
@@ -4,25 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!--JQUERY-->
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> 
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
 </head>
 <body>
-    <h1>Detalle Recolectores</h1>
-    <p><a href="/recolector">Recolectores</a></p>
-    <p><a href="/puntos">Puntos de Recolección</a></p>
+    <nav>
+        <div class="nav-wrapper  red darken-4">
+            <a href="#" class="brand-logo">Puntos asociados por recolector</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="/recolector">Recolectores</a></li>
+                <li><a href="/puntos">Puntos de Recolección</a></li>
+            </ul>
+        </div>
+    </nav>
 
-    <div>
-        
+
+    <div class="row">
+        <div class="col s10">
+
+        </div>
+    </div>
+
+
+    <div style="padding:10px;">
         @if(!is_null($recolectores))
             @foreach($recolectores as $r)
             <br>{{$r->idRecolector}} - <a href="/detalles/editar/{{$r->idRecolector}}">{{$r->nombre}}</a> <br>
-                Puntos de recoleccion asociados: <br>
-                @foreach($r->getPuntos() as $p)
-                    @if(!is_null($p))
-                        Punto: {{$p->idPunto}} ; Tipo de Basura: {{$p->tipo_de_basura}} <br>
-                        Direccion: {{$p->direccion}} <br><br>
-                    @endif
-                @endforeach
+            <div class="row">
+                <div class="col s6">
+                    <table>
+                        <thead>
+                            <tr>
+                            <th>idPunto</th>
+                            <th>Tipo de Basura</th>
+                            <th>Direccion</th>
+                            </tr>
+                        </thead>
 
+                        <tbody>
+                            
+                            @foreach($r->getPuntos() as $p)            
+                            @if(!is_null($p))
+                            <tr>
+                                <td>{{$p->idPunto}}</td>
+                                <td>{{$p->tipo_de_basura}}</td>
+                                <td>{{$p->direccion}}</td>
+                            </tr>
+                            @endif
+                            @endforeach
+                            
+                        </tbody>
+                    </table>
+                </div>    
+            </div>
             @endforeach
         @endif
     </div>
--- resources/views/recolector.blade.php
+++ resources/views/recolector.blade.php
@@ -4,29 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!--JQUERY-->
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> 
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </head>
 <body>
     <div>
-        <h1>-Recolectores-</h1>
-        <a href="/detalles">Detalles recolectores</a>
-
+        <nav>
+            <div class="nav-wrapper  red darken-4">
+                <a href="#" class="brand-logo">Recolectores</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="/detalles">Detalles recolectores</a></li>
+                </ul>
+            </div>
+        </nav>
+        
         <p>Registrar recolector:</p>
+        <div class="row">
+            <div class="col s4">
+                <form action="/recolector" method="POST">
+                    @csrf
+                    <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+                    <input type="text" name="dias" id="dias" placeholder="LMXJVSD">
+                    <input class="btn waves-effect waves-light" type="submit" value="Registrar">
+                </form>
+            </div>
+        </div>
 
-        <form action="/recolector" method="POST">
-            @csrf
-            <input type="text" name="nombre" id="nombre" placeholder="Nombre">
-            <input type="text" name="dias" id="dias" placeholder="LMXJVSD">
-            <input type="submit" value="Registrar">
-        </form>
+        <div class="row">
+            <div class="col s8 offset-s2">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Días de Recolección</th>
+                            <th></th>
+                        </tr>
+                    </thead>
 
-        @if(!is_null($recolectores))
-            @foreach($recolectores as $r)
-                <p>Nombre: {{$r->nombre}} <a href="/recolector/editar/{{$r->idRecolector}}">Modificar</a> 
-                                            <a href="/recolector/eliminar/{{$r->idRecolector}}">Eliminar</a>
-                <br> Días de recolección: {{$r->dias}} </p>
-            @endforeach
-        @endif
-
+                    <tbody>
+                    @if(!is_null($recolectores))
+                        @foreach($recolectores as $r)
+                        <tr>
+                            <td>{{$r->nombre}}</td>
+                            <td>{{$r->dias}}</td>
+                            <td><a href="/recolector/editar/{{$r->idRecolector}}">Modificar</a>/
+                            <a href="/recolector/eliminar/{{$r->idRecolector}}">Eliminar</a> </td>
+                        </tr>
+                        @endforeach
+                    @endif
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </body>
 </html>
